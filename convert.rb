3adeedc3a923a4fa6bf35a7553cf30f0cf1be092
jbfink <john.fink@gmail.com>
2014-02-19 21:38:59 -0500

    Stubbed out answer function.

--- convert.rb
+++ convert.rb
@@ -1,4 +1,9 @@
-# yeah
+def answer(line)
+	# here we take the "B-" lines and grep 'em for the answers - values like (3) - and return that value
+	# number = grep-one-char-between-parens
+	# return number
+end
+
 bank = File.new("single.txt", "r")
 bank.each do |line|
 	puts line
